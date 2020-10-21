426615b8fb71dc9d15d68b85fc4dcaa43ff150c4
AbhiDeveloper823 <mishraabhinav710@gmail.com>
2020-10-21 15:34:41 +0530

    Netflix

---
+++ build/precache-manifest.1933ee4291c997e60c5be3496f4b2ef0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fc2047ffefe357db29141880153d96f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "836ebff6ec2012ea7ec6",
+    "url": "/static/css/main.289d4849.chunk.css"
+  },
+  {
+    "revision": "b8ec68f11c23c92dcf65",
+    "url": "/static/js/2.d5dbdb9a.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.d5dbdb9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "836ebff6ec2012ea7ec6",
+    "url": "/static/js/main.4d62bfd5.chunk.js"
+  },
+  {
+    "revision": "07b33697149324db54e5",
+    "url": "/static/js/runtime-main.b8a87127.js"
+  }
+]);
